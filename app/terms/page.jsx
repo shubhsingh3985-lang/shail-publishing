88149855150c8e213ebb81a5900cf000a27e9076
Shubham Singh <shubhsingh3985@gmail.com>
2026-05-25 15:49:28 +0530

    remove empty pages/routes

---
+++ app/terms/page.jsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Header          from '@/components/layout/Header'
+import Footer          from '@/components/layout/Footer'
+
+export default function AboutPage() {
+
+  return (
+    <>
+      <Header />
+      <main>
+
+
+      </main>
+      <Footer />
+    </>
+  )
+}
